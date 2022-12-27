fb5f5c7053d286790590047618bafeb1411719b3
Thanh Phong <pttp1512@gmail.com>
2022-12-27 10:41:38 +0700

    Folder structure setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Pages
+import HomePage from "./pages/homepage";
+
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
